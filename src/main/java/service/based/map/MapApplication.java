bece56591f29ce42a5f7f9856e6e5da7bd398be6
Mert Kurt <tyrael91@hotmail.com>
2025-02-02 17:45:41 +0300

    Initialization of Project

---
+++ src/main/java/service/based/map/MapApplication.java
@@ -0,0 +1,13 @@
+package service.based.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
